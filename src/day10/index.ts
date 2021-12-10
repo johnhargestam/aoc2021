0d2a58848aa02aa0fbf05ecd90d379cb63744237
= <john.hargestam@gmail.com>
2021-12-10 18:02:57 +0100

    day 10 complete

--- src/day10/index.ts
+++ src/day10/index.ts
@@ -5,12 +5,12 @@ const parseInput = (rawInput: string) => rawInput.split(/\n/);
 const bracketsMatch = (a: string, b: string): boolean =>
   '([{<'.indexOf(a) === ')]}>'.indexOf(b);
 
-const scoreTable: {[key:string]: number} = {
+const scoreTable1: { [key: string]: number } = {
   ')': 3,
   ']': 57,
   '}': 1197,
   '>': 25137,
-}
+};
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
@@ -23,30 +23,63 @@ const part1 = (rawInput: string) => {
       if ('([{<'.includes(bracket)) {
         stack.push(bracket);
       } else {
-        const last = stack.pop();
-        if (last && !bracketsMatch(last, bracket)) {
-          score += scoreTable[bracket];
+        const unclosed = stack.pop();
+        if (unclosed && !bracketsMatch(unclosed, bracket)) {
+          score += scoreTable1[bracket];
           break;
         }
       }
-      
     }
   }
 
   return score;
 };
 
+const scoreTable2: { [key: string]: number } = {
+  '(': 1,
+  '[': 2,
+  '{': 3,
+  '<': 4,
+};
+
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+  const stacks: string[][] = input
+    .map((line) => {
+      const stack: string[] = [];
+
+      for (const bracket of line) {
+        if ('([{<'.includes(bracket)) {
+          stack.push(bracket);
+        } else {
+          const unclosed = stack.pop();
+          if (unclosed && !bracketsMatch(unclosed, bracket)) {
+            return undefined;
+          }
+        }
+      }
+      return stack;
+    })
+    .filter((stack): stack is string[] => stack !== undefined);
+
+  const scores = stacks.map((stack) => {
+    let score = 0;
+    for (let i = stack.length - 1; i >= 0; i--) {
+      score = score * 5 + scoreTable2[stack[i]];
+    }
+    return score;
+  });
+  scores.sort((a, b) => a - b);
+
+  return scores[Math.floor(scores.length / 2)];
 };
 
 run({
   part1: {
     tests: [
       {
-         input: `[({(<(())[]>[[{[]{<()<>>
+        input: `[({(<(())[]>[[{[]{<()<>>
 [(()[<>])]({[<{<<[]>>(
 {([(<{}[<>[]}>{[]{[(<()>
 (((({<>}<{<{<>}{[]{[]{}
@@ -74,11 +107,11 @@ run({
 [<(<(<(<{}))><([]([]()
 <{([([[(<>()){}]>(<<{{
 <{([{{}}[<[[[<>{}]]]>[]]`,
-       expected: 288957,
-     },
+        expected: 288957,
+      },
     ],
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: true,
+  onlyTests: false,
 });
