3a31eb927cbe715c6d5b7649abe060db7766b778
= <john.hargestam@gmail.com>
2021-12-12 14:02:36 +0100

    day 12 part 2 complete

--- src/day12/index.ts
+++ src/day12/index.ts
@@ -28,7 +28,7 @@ const typeOfName = (name: string): Type => {
   return Type.BIG;
 };
 
-const parseInput = (rawInput: string): Connection[] =>
+const parseInput = (rawInput: string): Connection[] => 
   rawInput.split(/\n(?:\s+)?/).map(
     (connection) =>
       connection.split(/-/).map((name) => ({
@@ -62,13 +62,42 @@ const explore = (connections: Connection[], pastCaves: Cave[]) => {
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return explore(input, [{name: 'start', type: Type.START}]);
+  return explore(input, [{ name: 'start', type: Type.START }]);
+};
+
+const canVisit2 = (cave: Cave): boolean =>
+  cave.type !== Type.START;
+
+const exceededVisits = (pastCaves: Cave[]): boolean => {
+  const smallCaves = pastCaves
+    .filter((c) => c.type === Type.SMALL)
+    .map((c) => c.name);
+  return new Set(smallCaves).size + 1 < smallCaves.length;
+}
+
+const explore2 = (connections: Connection[], pastCaves: Cave[]) => {
+  const [position] = pastCaves.slice(-1);
+  if (position.type === Type.END) {
+    return 1;
+  }
+  if (exceededVisits(pastCaves)) {
+    return 0;
+  }
+  let paths = 0;
+  for (const [a, b] of connections) {
+    if (a.name === position.name && canVisit2(b)) {
+      paths += explore2(connections, pastCaves.concat(b));
+    } else if (b.name === position.name && canVisit2(a)) {
+      paths += explore2(connections, pastCaves.concat(a));
+    }
+  }
+  return paths;
 };
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+  return explore2(input, [{ name: 'start', type: Type.START }]);
 };
 
 run({
@@ -123,10 +152,50 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: 0
-      // },
+      {
+        input: `start-A
+        start-b
+        A-c
+        A-b
+        b-d
+        A-end
+        b-end`,
+        expected: 36,
+      },
+      {
+        input: `dc-end
+        HN-start
+        start-kj
+        dc-start
+        dc-HN
+        LN-dc
+        HN-end
+        kj-sa
+        kj-HN
+        kj-dc`,
+        expected: 103,
+      },
+      {
+        input: `fs-end
+        he-DX
+        fs-he
+        start-DX
+        pj-DX
+        end-zg
+        zg-sl
+        zg-pj
+        pj-he
+        RW-he
+        fs-DX
+        pj-RW
+        zg-RW
+        start-pj
+        he-WI
+        zg-he
+        pj-fss
+        start-RW`,
+        expected: 3509,
+      },
     ],
     solution: part2,
   },
