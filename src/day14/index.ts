12e28ad3a1ffdebf37d4f7f984f869c54deac579
= <john.hargestam@gmail.com>
2021-12-14 21:22:22 +0100

    day 14 part 1 improvements

--- src/day14/index.ts
+++ src/day14/index.ts
@@ -5,41 +5,59 @@ const parseInput = (rawInput: string) => {
   return {
     template,
     rules: rules.split(/\n/).map((rule) => {
-      const [pair, insert] = rule.split(/ -> /);
-      return { pair, insert };
+      const [match, insert] = rule.split(/ -> /);
+      return { match, insert };
     }),
   };
 };
 
-const part1 = (rawInput: string) => {
-  const input = parseInput(rawInput);
-  let { template } = input;
+interface Letters {
+  [letter: string]: number;
+}
 
-  for (let steps = 1; steps <= 10; steps++) {
-    let newTemplate = template[0];
-    for (let c = 0; c < template.length + 1; c++) {
-      const [t1, t2] = template.slice(c, c + 2);
-      for (const { pair, insert } of input.rules) {
-        const [r1, r2] = pair;
-        if (t1 === r1 && t2 === r2) {
-          newTemplate += `${insert}${t2}`;
-          break;
-        }
-      }
-    }
-    template = newTemplate;
+interface Rule {
+  match: string;
+  insert: string;
+}
+
+const polymerize = (
+  letters: Letters,
+  pair: string,
+  rules: Rule[],
+  times: number,
+) => {
+  if (times <= 0) {
+    return;
   }
-  const map = template.split('').reduce((map: {[key: string]: number}, letter) => {
-    map[letter] = 1 + (map[letter] || 0);
-    return map;
+  for (const { match, insert } of rules) {
+    if (match === pair) {
+      letters[insert] = 1 + (letters[insert] || 0);
+      polymerize(letters, `${pair.at(0)}${insert}`, rules, times - 1);
+      polymerize(letters, `${insert}${pair.at(1)}`, rules, times - 1);
+      return;
+    }
+  }
+};
+
+const part1 = (rawInput: string) => {
+  const { template, rules } = parseInput(rawInput);
+
+  const letters = template.split('').reduce((letters: Letters, c) => {
+    letters[c] = 1 + (letters[c] || 0);
+    return letters;
   }, {});
-  const counts = Object.keys(map).map(key => map[key]);
+  for (let c = 0; c < template.length + 1; c++) {
+    const pair = template.slice(c, c + 2);
+    polymerize(letters, pair, rules, 10);
+  }
+  const counts = Object.keys(letters).map((c) => letters[c]);
 
   return Math.max(...counts) - Math.min(...counts);
 };
 
 const part2 = (rawInput: string) => {
   const { template, rules } = parseInput(rawInput);
+
   return;
 };
 
@@ -72,30 +90,29 @@ run({
   },
   part2: {
     tests: [
-      {
-        input: `        NNCB
-
-        CH -> B
-        HH -> N
-        CB -> H
-        NH -> C
-        HB -> C
-        HC -> B
-        HN -> C
-        NN -> C
-        BH -> H
-        NC -> B
-        NB -> B
-        BN -> B
-        BB -> N
-        BC -> B
-        CC -> N
-        CN -> C`,
-        expected: 1588,
-      },
+      // {
+      //   input: `        NNCB
+      //   CH -> B
+      //   HH -> N
+      //   CB -> H
+      //   NH -> C
+      //   HB -> C
+      //   HC -> B
+      //   HN -> C
+      //   NN -> C
+      //   BH -> H
+      //   NC -> B
+      //   NB -> B
+      //   BN -> B
+      //   BB -> N
+      //   BC -> B
+      //   CC -> N
+      //   CN -> C`,
+      //   expected: 1588,
+      // },
     ],
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: true,
+  onlyTests: false,
 });
